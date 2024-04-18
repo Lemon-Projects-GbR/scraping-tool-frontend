c0fe7ef6bd45bce80d2e533596002bd0e4bebe01
Cristian Dias de Castro <cdias@debian.com>
2024-04-18 20:49:19 +0200

    incr

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
+import { Box, FormControl, TextField, Button } from "@mui/material";
 import type { MetaFunction } from "@remix-run/node";
+import { useEffect } from "react";
+import DataSchemaTextField from "../components/DataSchemaTextField";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +10,80 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+/* 
+TODO: Siehe unten
+Convert this String:
+
+  attribute1: value1
+  attribute2:
+    attribute3: value3
+    attribute4: value4
+    attribute5:
+      attribute6: value6
+      attribute7: value7
+  attribute8: value8
+  
+
+to this Object:
+  
+    {
+      attribute1: "value1",
+      attribute2: {
+        attribute3: "value3",
+        attribute4: "value4",
+        attribute5: {
+          attribute6: "value6",
+          attribute7: "value7",
+        },
+      },
+      attribute8: "value8",
+    } 
+*/
+
+function parseStringToObject(str: string) {
+  //
+  let obj = str;
+  return obj;
+}
+
+// TODO: Multiple URLs
+
 export default function Index() {
+  const str = `
+attribute1: value1
+ attribute2:
+   attribute3: value3
+   attribute4: value4
+   attribute5:
+     attribute6: value6
+     attribute7: value7
+attribute8: value8
+`;
+
+  useEffect(() => {
+    console.log(parseStringToObject(str));
+  }, []);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+      <FormControl>
+        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+          <Box
+            sx={{ padding: "1rem", display: "flex", flexDirection: "column" }}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            <TextField label="URL" variant="outlined" />
+            <Button variant="contained">Add URL</Button>
+            <Box sx={{ display: "flex" }}>
+              <TextField label="Attibute" variant="outlined" id="attr1" />
+              <TextField label="Selector" variant="outlined" id="sel1" />
+            </Box>
+            <Button variant="contained">Add Attribute</Button>
+          </Box>
+          <Box sx={{ padding: "1rem" }}>
+            <DataSchemaTextField />
+          </Box>
+        </Box>
+      </FormControl>
     </div>
   );
 }
